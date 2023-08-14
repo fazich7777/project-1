7658ce6f744ab1410bec8b7768036ff63b17ac6f
fazich <sasharovnerpro@gmail.com>
2023-08-14 17:03:04 +0300

    lvl1

--- fazich.py
+++ fazich.py
@@ -1,1 +1,2 @@
-#ggg
+question1 = input('You are a hero and you see a hostile ogre what you use to defeat him. ANSWER OPTIONS: 1-mech2-try to ask him to leave')
+print('b')if question1 == 'sword' else print('g')
