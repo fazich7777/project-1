1367220f31417b0835f22cf418f11c94285fb8eb
alinlob <alinalobova17@gmail.com>
2023-08-18 19:40:21 +0300

    add .

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.him')
+    return render_template('index.html')
 
 @app.route('/result', methods=['POST'])
 def result():
@@ -14,12 +14,12 @@ def result():
     
     result = ""
     
-    if question1 == 'sword' and question2 == 'try to kill him' and question3 == 'give them acorns that happened to be in your pocket':
-        result = 'you win!'
+    if question1 == 'Mech' and question2 == 'Try to kill him' and question3 == 'Give them acorns that happened to be in your pocket':
+        result = 'You win!'
     else:
-        result = 'you losw.'
+        result = 'You lose.'
         
     return render_template('result.html', result=result)
 
-if __name__=='++main++':
-    app.run(debug=true)
+if __name__ == '__main__':
+    app.run(debug=True)
